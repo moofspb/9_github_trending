c6062a1ef1498bf684e0f3bcff8f5025d0054568
Mikhail Kuntsevich <moofspb@gmail.com>
2017-03-17 02:50:39 +0300

    Add argparse argument and module description

--- github_trending.py
+++ github_trending.py
@@ -3,7 +3,7 @@ import argparse
 import requests
 
 
-def get_trending_repositories(top_size):
+def get_trending_repositories(top_repos_amount):
     week = date.today() - timedelta(days=7)  # for check new repos less than 1 week old
     api_url = 'https://api.github.com/search/repositories'
     parameters = {
@@ -11,18 +11,23 @@ def get_trending_repositories(top_size):
         'sort': 'stars',
     }
     all_repositories = requests.get(api_url, params=parameters).json()
-    trending_repositories = all_repositories['items'][:top_size]
+    trending_repositories = all_repositories['items'][:top_repos_amount]
     return trending_repositories
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('top_size', default=20, type=int,
+    parser = argparse.ArgumentParser(description='Script find the most trending new/ '
+                                                 'repositories on github.com')
+    parser.add_argument('--repos_amount',
+                        type=int,
+                        const=20,
+                        default=20,
+                        nargs='?',
                         help='Amount of trending repositories')
     args = parser.parse_args()
-    trending_repos = get_trending_repositories(args.top_size)
+    trending_repos = get_trending_repositories(args.repos_amount)
     print('There is {} most trending repositories on Github.com for the last week:'.format(
-        args.top_size)
+        args.repos_amount)
     )
     for repo in trending_repos:
         print('Repository: {}, issues: {}, link: "https://github.com/{}"'.format(
