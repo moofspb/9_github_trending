7f7f05ab8bfda12336e2de2b0d78ce7b649dab10
Mikhail Kuntsevich <moofspb@gmail.com>
2017-03-17 01:30:01 +0300

    Add function get_trending_repositories and __main__ block

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,31 @@
+from datetime import date, timedelta
+import argparse
+import requests
 
 
 def get_trending_repositories(top_size):
-    pass
+    week = date.today() - timedelta(days=7)  # for check new repos less than 1 week old
+    api_url = 'https://api.github.com/search/repositories'
+    parameters = {
+        'q': 'created:>={}'.format(str(week)),
+        'sort': 'stars',
+    }
+    all_repositories = requests.get(api_url, params=parameters).json()
+    trending_repositories = all_repositories['items'][:top_size]
+    return trending_repositories
 
-
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('top_size', default=20, type=int,
+                        help='Amount of trending repositories')
+    args = parser.parse_args()
+    trending_repos = get_trending_repositories(args.top_size)
+    print('There is {} most trending repositories on Github.com for the last week:'.format(
+        args.top_size)
+    )
+    for repo in trending_repos:
+        print('Repository: {}, issues: {}, link: "https://github.com/{}"'.format(
+            str(repo['name']), repo['open_issues_count'],
+            repo['full_name'])
+        )
