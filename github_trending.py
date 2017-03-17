37c27132200a15505e73244b4c9a62b80a298541
Mikhail Kuntsevich <moofspb@gmail.com>
2017-03-17 14:59:27 +0300

    Remove magic number, delete useless comment, add two functions (get_script_args and print_trending_repositories),  refactor function get_trending_repositories

--- github_trending.py
+++ github_trending.py
@@ -3,19 +3,7 @@ import argparse
 import requests
 
 
-def get_trending_repositories(top_repos_amount):
-    week = date.today() - timedelta(days=7)  # for check new repos less than 1 week old
-    api_url = 'https://api.github.com/search/repositories'
-    parameters = {
-        'q': 'created:>={}'.format(str(week)),
-        'sort': 'stars',
-    }
-    all_repositories = requests.get(api_url, params=parameters).json()
-    trending_repositories = all_repositories['items'][:top_repos_amount]
-    return trending_repositories
-
-
-if __name__ == '__main__':
+def get_script_args():
     parser = argparse.ArgumentParser(description='Script find the most trending new/ '
                                                  'repositories on github.com')
     parser.add_argument('--repos_amount',
@@ -25,12 +13,33 @@ if __name__ == '__main__':
                         nargs='?',
                         help='Amount of trending repositories')
     args = parser.parse_args()
-    trending_repos = get_trending_repositories(args.repos_amount)
-    print('There is {} most trending repositories on Github.com for the last week:'.format(
-        args.repos_amount)
-    )
-    for repo in trending_repos:
+    return args.repos_amount
+
+
+def get_trending_repositories(top_repos_amount, repo_creation_period=7):
+    days_ago = date.today() - timedelta(days=repo_creation_period)
+    api_url = 'https://api.github.com/search/repositories'
+    parameters = {
+        'q': 'created:>={}'.format(str(days_ago)),
+        'sort': 'stars',
+        'per_page': top_repos_amount
+    }
+    trending_repositories = requests.get(api_url, params=parameters).json()['items']
+    return trending_repositories
+
+
+def print_trending_repositories(trending_repositories):
+    for repo in trending_repositories:
         print('Repository: {}, issues: {}, link: "https://github.com/{}"'.format(
             str(repo['name']), repo['open_issues_count'],
             repo['full_name'])
         )
+
+
+if __name__ == '__main__':
+    repos_amount = get_script_args()
+    trending_repos = get_trending_repositories(repos_amount)
+    print('There is {} most trending repositories on Github.com for the last week:'.format(
+        repos_amount)
+    )
+    print_trending_repositories(trending_repos)
